a44eb6f56db5b0215690a29be62d08af1e2f41ac
Bojan <bojanj6m@gmail.com>
2017-02-28 20:32:11 -0500

    Index Routes with React Route

--- ReduxBlog/src/routes.js
+++ ReduxBlog/src/routes.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-
-
-const Greeting = () => {
-  return (
-    <div>Hey there!</div>
-  );
-}
+import PostIndex from './components/post_index';
 
 
 // Nested routes will be passed to this.props.children
+// Note the differnce between the Route and IndexRoute
 export default (
   <Route path="/" component={App} >
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
+    <IndexRoute component={PostIndex} />
   </Route>
 );
