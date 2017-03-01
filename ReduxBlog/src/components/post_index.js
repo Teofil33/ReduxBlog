9f573f24d624df938dac2fdd19cd0fe41cfa6f42
Bojan <bojanj6m@gmail.com>
2017-02-28 21:45:51 -0500

    Fetching Data with Lifecycle methods

--- ReduxBlog/src/components/post_index.js
+++ ReduxBlog/src/components/post_index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of blog posts.</div>;
-};
+// componentWillMount is a life cycle method, it will be called automatically
+// by React whenever our component is about to be rendered to the DOM
+// for the first time, it will not be called on subsuquesent re-renders
+// Great place to place our action creator to fetch data
+class PostIndex extends Component {
+  componentWillMount() {
+    console.log("Good time to call action creator");
+  }
+  render () {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
