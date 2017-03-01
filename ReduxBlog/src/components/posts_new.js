2255311f46edad6809f0b70a408aa693c54bc303
Bojan <bojanj6m@gmail.com>
2017-03-01 16:47:20 -0500

    Display a Form

--- ReduxBlog/src/components/posts_new.js
+++ ReduxBlog/src/components/posts_new.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render () {
     return (
-    <div>Create Form</div>
+    <form className="col-sm-8 col-sm-offset-2">
+      <h3>Create a New Post</h3>
+      <div className="form-group">
+        <label>Title</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Category</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Content</label>
+        <textarea className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-info btn-block">Create</button>
+    </form>
     );
   };
 }
 
-export default PostsNew;
+export default reduxForm ({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+// https://github.com/erikras/redux-form
+// npm install --save redux-form
