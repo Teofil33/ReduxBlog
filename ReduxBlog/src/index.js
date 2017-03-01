7d72424f60469444e832f56afb06d11fa3079adb
Bojan <bojanj6m@gmail.com>
2017-02-28 20:13:38 -0500

    Route configuration, Nested Routes

--- ReduxBlog/src/index.js
+++ ReduxBlog/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import reducers from './reducers';
+import routes from './routes';
 
 // browserHistory, hashHistory (after #) , memoryHistory
 // Whenever url updates react-router is going to interpret everything after
 // the protocol, after http://www.blog.com/posts/5 after http://www.blog.com/
 
-import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
