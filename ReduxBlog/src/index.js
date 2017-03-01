ccc0837a6dfaaeb9544cc859f803648855d66cb3
Bojan <bojanj6m@gmail.com>
2017-02-28 19:51:14 -0500

    Router initial setup

--- ReduxBlog/src/index.js
+++ ReduxBlog/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
+// browserHistory, hashHistory (after #) , memoryHistory
+// Whenever url updates react-router is going to interpret everything after
+// the protocol, after http://www.blog.com/posts/5 after http://www.blog.com/
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
